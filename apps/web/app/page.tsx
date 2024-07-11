bc28e1e5237f22bffa6a383c9fd0b4db8243408c
hosenur <hosenur.dev@gmail.com>
2024-07-12 04:43:03 +0530

    init shadcn as shared repo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { prisma } from '../lib/database'
+import { Button } from "@repo/ui/components/ui/button"
+
 
 export default async function Home() {
   const kv = await prisma.kV.findFirst()
   return (
     <div>
+      <div className='bg-red-500'>
+        hi
+
+      </div>
       {JSON.stringify(kv)}
+      <Button>Hello</Button>
     </div>
   )
 }
