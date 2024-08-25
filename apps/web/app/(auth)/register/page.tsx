b979dab28302278c469ef8a3b78ee51da15b92cb
hosenur <hosenur.dev@gmail.com>
2024-08-25 08:12:56 +0530

    rename eslint package name

--- apps/web/app/(auth)/register/page.tsx
+++ apps/web/app/(auth)/register/page.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link"
 
-import { Button } from "@repo/ui/components/ui/button"
+import RegisterForm from "@/components/register/register-form"
 import {
     Card,
     CardContent,
@@ -8,9 +7,6 @@ import {
     CardHeader,
     CardTitle,
 } from "@repo/ui/components/ui/card"
-import { Input } from "@repo/ui/components/ui/input"
-import { Label } from "@repo/ui/components/ui/label"
-import RegisterForm from "@/components/register/register-form"
 
 export default function RegisterPage() {
     return (
